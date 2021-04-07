bb4693c5a0dab35e31aff8db52ee5ec16f32fb89
Marco Antonio <marco@spnonus.local>
2021-04-07 17:29:41 -0300

    Profile Authorization Logic

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 
 class ProfilesController extends Controller
 {
     public function show(User $user)
     {
         return view('profiles.show', compact('user'));
+    }
+
+    public function edit(User $user)
+    {
+        return view('profiles.edit', compact('user'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/tweets', 'TweetsController@index')->name('home');
     Route::post('/tweets', 'TweetsController@store');
     Route::post('/profiles/{user:name}/follow', 'FollowsController@store');
+    Route::get('/profiles/{user:name}/edit', 'ProfilesController@edit')->middleware('can:edit,user');
 });
 
 //Route::get('/profiles/{user}', 'ProfilesController@show')->name('profile');
